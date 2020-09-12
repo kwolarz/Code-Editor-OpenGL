01093bcc2ecaef42965ca1f250cd70419554dac8
Krzysztof Wolarz <krzysztofwolarz@gmail.com>
2020-09-12 20:37:48 +0200

    add comments and small fixes to code

--- editor.py
+++ editor.py
@@ -3,31 +3,33 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import os
 
-
-# głowna lista odpowiadająca za ilość wierszy w edytorze
+# ------------- Zmienne globalne ------------------
+# głowna lista programu (lista list), zawiera w sobie listy odpowiadające za dany wiersz
+# ilosc tych list odpowiada ilości wierszy w programie
 characters = []
-characters.append([])
+characters.append([]) # dodawnia pierwszego wiersza
 
-# lista w której zapisywane są pozycje słów kluczowych
+# lista w której zapisywane są pozycje słów kluczowych (lista list), zawiera w sobie listy odpowiadające za dany wiersz
+# ilosc tych list odpowiada ilości wierszy w programie
 keywordPositions = []
 keywordPositions.append([])
 
 # zmienne odpowaidające za szerokość i wysokość okna
-w,h= 800, 600
+w, h= 800, 600
 
 # aktualna liczba wierszy edytora
 row = 0
 
-# aktualna liczba kolumn w danym wierszu edytora
-column = 0
+# ostatni widoczny wiersz edytora - uzywane do obsługi scrollowania
 lastVisisbleRow = 39
 
 # pozycja kursora - wiersze
 coursorRow = row
 
 # pozycja kursora - kolumny
-coursorColumn = column
+coursorColumn = 0
 
+# ------------- Obsługa plików ------------------
 def saveFile():
     global row
     file = open('file.txt', 'w+')
@@ -43,29 +45,30 @@ def saveFile():
 def openFile():
     global row
 
-
-    if os.path.isfile('file.txt'):
+    if os.path.isfile('file.txt'):       # sprawdzenie czy taki plik istnieje, jeśli nie - tworzy taki plik
         characters.clear()
         keywordPositions.clear()
+
         with open('file.txt', 'r') as file:
-            if os.stat('file.txt').st_size == 0:
+            if os.stat('file.txt').st_size == 0:   # w przypadku gdy plik jest pusty dodawana jest pusta lista
                 characters.append([])
                 keywordPositions.append([])
+
             else:
                 for i, line in enumerate(file):
                     characters.append([])
                     characters[i] = list(line.replace('\n', ''))
                     keywordPositions.append([])
-                    # for coursorRow in range(row):
                     checkIfSyntax(listToString(characters[i]), i)
                     specialCharSyntax(listToString(characters[i]), i) 
                     row = i
 
             file.close()
+
     else:
         file = open('file.txt', 'w+')
 
-
+# ------------- Podświetlanie składni kodu ------------------
 # metoda odpoiwadająca za zamianę listy char'ów w jeden string
 def listToString(listOfChar):
     word = ''
@@ -85,6 +88,9 @@ def find_all(a_str, sub):
 
 
 # metoda odpowiadająca za znajdowanie słów kluczowych w danym stringu
+# metoda dostaje cały wiersz, szuka w nim począteku oraz końca słowa kluczowego
+# następnie sprawdza czy słowo kluczowe nie jest częścią jakiegoś wyrazu 
+# metoda kończy się dodaniem pozycji liter do listy pozycji słów kluczowych w danym wierszu
 def checkIfSyntax(text, row):
     keywordPositions[row].clear()
     keywords = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
@@ -99,7 +105,7 @@ def checkIfSyntax(text, row):
                         keywordPositions[row].append(i)
 
             
-
+# metoda odpowiadająca za znajdowaanie pojedyńczych znaków do podświetlenia
 def specialCharSyntax(text, row):
     keywords = [':', ';', '"', "'", '[', ']', '{', '}', '(', ')', '=']
     # startOfKeyword = None
@@ -109,7 +115,7 @@ def specialCharSyntax(text, row):
         keywordPositions[row].extend(positions)
 
 
-
+# ------------- Obsługa zachowań po naciśnięciu przycisków ------------------
 # metoda odpowaidająca za obsługę klawiszy kierunkowych na klawiaturze
 def arrowHandler(key, x, y):
     global coursorRow, row, coursorColumn, h, lastVisisbleRow
@@ -117,19 +123,17 @@ def arrowHandler(key, x, y):
     if key == GLUT_KEY_UP:
         if coursorRow > 0:
             coursorRow -= 1
-            if lastVisisbleRow > 39 and coursorRow < lastVisisbleRow - 38:
+            if lastVisisbleRow > 39 and coursorRow < lastVisisbleRow - 38:  # obsługa scrollowania
                 h -= 15
                 lastVisisbleRow -= 1
+
             if coursorColumn > len(characters[coursorRow]):
                 coursorColumn = len(characters[coursorRow])
 
     elif key == GLUT_KEY_DOWN:
         if row > coursorRow:
-            # if row > 38:
-            #     if coursorRow == row - 1:
-            #         h += 15
 
-            if row > lastVisisbleRow:
+            if row > lastVisisbleRow:           # obsługa scrollowania
                 if coursorRow >= lastVisisbleRow - 1:
                     h += 15
                     lastVisisbleRow += 1
@@ -154,18 +158,18 @@ def arrowHandler(key, x, y):
 
 # metoda odpowiadająca za obsługę klawiatury
 def handler(key, x, y):
-    global row, column, coursorRow, coursorColumn, h, lastVisisbleRow
+    global row, coursorRow, coursorColumn, h, lastVisisbleRow
     
-
-    if key == b'\x7f':
+    if key == b'\x7f':           # usuwanie znaków (backspace)
         if coursorColumn > 0:
             coursorColumn -= 1 
+
         if len(characters[coursorRow]) == 0:
             if row > 0:
                 characters.pop(coursorRow)
                 keywordPositions.pop(coursorRow)
                 row -= 1
-                if row: 
+                if row or coursorRow > row: 
                     coursorRow -= 1
                 coursorColumn = len(characters[coursorRow])
                 
@@ -173,8 +177,7 @@ def handler(key, x, y):
             characters[coursorRow].pop(coursorColumn)
             keywordPositions[coursorRow].pop(coursorColumn)
 
-    elif key==b'\r':
-        # if row > coursorRow:
+    elif key==b'\r':            # nowa linia (enter/return)
         if row > lastVisisbleRow - 1:
             if coursorRow == row:
                 h += 15
@@ -188,16 +191,18 @@ def handler(key, x, y):
         characters[coursorRow - 1] = characters[coursorRow - 1][:coursorColumn]
         coursorColumn = 0
 
-    elif key == b'\t':
+    elif key == b'\t':        # tabulator
         for _ in range(4):
             characters[coursorRow].insert(coursorColumn, ' ')
             coursorColumn += 1
 
-    elif key == b'\x13':
+    elif key == b'\x13':      # kombinacja CTRL + f
         saveFile()
-    elif key == b'\x0f':
+
+    elif key == b'\x0f':      # kombinacja CTRL + s
         openFile()
-    else:
+
+    else:                     # kazdy inny znak zostanie dodany do listy oraz narysowany
         characters[coursorRow].insert(coursorColumn, key.decode('UTF-8'))
         coursorColumn += 1
 
@@ -208,7 +213,7 @@ def handler(key, x, y):
 
     return None
 
-
+# ------------- Rysowanie elementów na ekranie ------------------
 # metoda odpowaidająca za rysowanie tekstu w oknie edytora
 def glut_print(x, y, font, text, r, g, b):
     blending = False 
@@ -225,6 +230,7 @@ def glut_print(x, y, font, text, r, g, b):
         glDisable(GL_BLEND) 
 
 
+# zmienna słuąca do stworzenia animowanego kursora
 index = 0
 # metoda odpowiadająca za rysowanie elemntów w oknie edytora
 def showScreen():
@@ -240,14 +246,12 @@ def showScreen():
     # rysowanie znaków na bazie listy
     for charRow in range(row+1):
         for pos, char in enumerate(characters[charRow]):
-            
             if pos in keywordPositions[charRow]:
                 glut_print(33 + 10*pos, (h - 15) - (15 * (charRow)), GLUT_BITMAP_9_BY_15, char, 0, 0.7, 0.95)
             else:
                 glut_print(33 + 10*pos, (h - 15) - (15 * (charRow)), GLUT_BITMAP_9_BY_15, char, 1.0 , 1.0 , 1.0)
 
-    # rysowanie kursora
-    # glut_print(20 + 10*len(characters[coursorRow]), (h - 15) - (15 * (coursorRow)), GLUT_BITMAP_HELVETICA_18, "|", 0.5 , 0.5 , 0.5)
+    # rysowanie oraz animizacja kursora
     if index > 0 and index < 40:
         glut_print(33 + 10*coursorColumn, (h - 15) - (15 * (coursorRow)), GLUT_BITMAP_HELVETICA_18, "|", 0 , 0 , 0)
     elif index == 80:
@@ -257,6 +261,7 @@ def showScreen():
 
     glutSwapBuffers()
 
+# ------------- Inicjalizacja środowiska ------------------
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_RGBA)
